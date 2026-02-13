2940b30b036f319f52324771bfb9df198a4afcd3
Oguri Cap <129611515+qiqd@users.noreply.github.com>
2026-02-13 16:32:07 +0800

    fix:修改github工作流文件

--- src/main/java/org/holo/service/AppSettingHistoryService.java
+++ src/main/java/org/holo/service/AppSettingHistoryService.java
@@ -5,22 +5,19 @@ import org.holo.content.UserContent;
 import org.holo.entity.AppSetting;
 import org.holo.repo.AppSettingRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class AppSettingHistoryService {
-  private final AppSettingRepository appSettingRepository;
-  private final UserContent userContent;
+    private final AppSettingRepository appSettingRepository;
+    private final UserContent userContent;
 
-  public void saveAppSetting(AppSetting appSetting) {
-    appSetting.setUserId(userContent.getUserId());
-   appSetting.setUserId(userContent.getUserId());
-    appSettingRepository.save(appSetting);
-  }
+    public void saveAppSetting(AppSetting appSetting) {
+        appSetting.setUserId(userContent.getUserId());
+        appSetting.setUserId(userContent.getUserId());
+        appSettingRepository.save(appSetting);
+    }
 
-  public AppSetting queryAppSetting() {
-    return appSettingRepository.queryFirstByUserId(userContent.getUserId());
-  }
+    public AppSetting queryAppSetting() {
+        return appSettingRepository.queryFirstByUserId(userContent.getUserId());
+    }
 }
